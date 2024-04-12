751dc02dc30da10f03648c94eb636d71e8d3d45e
arvind <a.arvind@rifluxyss.com>
2024-04-12 18:58:34 +0530

    DB added

---
+++ config/dbConnect.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const connection = {};
+
+const connectToDB = async () => {
+  try {
+    if (connection.isConnected) {
+      console.log('connection already exist');
+      return;
+    }
+
+    console.log('connecting to DB');
+    const db = await mongoose.connect(process.env.DATABASE_URL);
+    console.log('connected to DB');
+    connection.isConnected = db.connection;
+  } catch (error) {
+    console.log(' failed to connect DB: ', error);
+  }
+};
+
+export default connectToDB;
--- server.js
+++ server.js
@@ -13,17 +13,19 @@ import { corsOptions } from './config/cors.config.js';
 import logEvent, { emitFunc } from './middleware/logEvents.js';
 import errorHandler, { serve404 } from './middleware/errorHandler.js';
 import { verifyJWT } from './middleware/verifyJWT.js';
+import mongoose from 'mongoose';
+import connectToDB from './config/dbConnect.js';
 class Emitter extends EventEmitter {}
 const emitter = new Emitter();
+connectToDB();
 
 const app = express();
 const port = process.env.PORT || 1234;
 emitter.on('log', (msg, fName) => logEvent(msg, fName));
-
 app.use((req, res, next) => emitFunc(req, res, next, emitter));
 app.use(cors(corsOptions));
-app.use(express.urlencoded());
-app.use(express.json());
+// app.use(express.urlencoded());
+// app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('./public')); //# middleware for static files
 app.use('/', rootRouter);
@@ -36,8 +38,10 @@ app.use(verifyJWT);
 app.use('/employee(s)?', employeesRouter);
 app.all('*', [mid1, mid2, (req, res) => serve404(req, res, emitter)]); //# custom middleware
 app.use(errorHandler);
-
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+mongoose.connection.once('open', () => {
+  console.log('DB Connected');
+  app.listen(port, () => console.log(`Server is running on port ${port}`));
+});
 
 function mid1(req, res, next) {
   console.log('error logged');
@@ -48,4 +52,4 @@ function mid2(req, res, next) {
   next();
 }
 
-//TODO: add credencials middleware, set secure true
+//TODO: add credencials middleware, set secure true. (more on TODO before frontend.txt)
